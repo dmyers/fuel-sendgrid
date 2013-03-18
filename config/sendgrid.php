5f32cc62b690f69f6fb64d8ada40f6a1e7692547
Derek Myers <arcticpro@gmail.com>
2013-03-18 00:28:10 -0500

    Added default sendgrid package config file

---
+++ config/sendgrid.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+	/**
+	 * username - The username to use for API calls.
+	 */
+	'username' => 'your_username',
+	
+	/**
+	 * password - The password to use for API calls.
+	 */
+	'password' => 'your_password',
+	
+	/**
+	 * name - The name to send email from.
+	 * email - The email to send email from.
+	 *
+	 * Leave them empty to not override default
+	 */
+	'from' => array(
+		'name'  => false,
+		'email' => false,
+	),
+);
